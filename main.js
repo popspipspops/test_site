e1e572187192bcb2a18b7c06b459d63b3d85d60c
Kirill Zhatkin <100416404+popspipspops@users.noreply.github.com>
2022-11-14 18:30:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ window.onload = () => {
    let options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.5
+      threshold: 0.3
   }
 
+
+
+  
+
+//   ОБС для гридов
   const observer = new IntersectionObserver((entries, observer) => {
    entries.forEach(entry => {
       if (entry.isIntersecting) {
          const targ = entry.target
          console.log(targ)
-         targ.classList.add('bg-sky-100')
+         targ.classList.add('bg-white')
          observer.unobserve(targ)
       }
    })
